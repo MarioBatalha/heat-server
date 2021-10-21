8a17ea413916ee66dfcd7be03ecf33031e649a60
MarioBatalha <mariobatalha9@gmail.com>
2021-10-22 00:10:22 +0100

    add model user

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,24 +1,41 @@
-import { Response } from "express";
+import { response, Response } from "express";
 import axios from "axios";
 
 interface IAccessTokenResponse {
   access_token: string;
 }
 
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
 class AuthenticateUserService {
   async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
 
-    const response = await axios.post<IAccessTokenResponse>(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
-      },
-      headers: {
-        accept: "application/json",
-      },
-    });
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          accept: "application/json",
+        },
+      });
+
+    const response = await axios.get<IUserResponse>(
+      "https://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      }
+    );
     return response.data;
   }
 }
