511ea5d9ab6d62e8bd99e81f9f0a5ffcfc0e6759
MarioBatalha <mariobatalha9@gmail.com>
2021-10-21 20:21:50 +0100

    code base

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,26 @@
+import { Response } from "express";
+import axios from "axios";
+
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    const response = await axios.post<IAccessTokenResponse>(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      headers: {
+        accept: "application/json",
+      },
+    });
+    return response.data;
+  }
+}
+
+export { AuthenticateUserService };
