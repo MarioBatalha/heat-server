d44084c97f8bfaff4766bedb09bd3688094e96cc
MarioBatalha <mariobatalha9@gmail.com>
2021-10-22 01:00:12 +0100

    add CreateMessageServices and message model

---
+++ src/services/CreateMessageService.ts
@@ -0,0 +1,13 @@
+
+
+class CreateMessageService {
+    async execute(text: string, user_id: string) {
+        try {
+            
+        } catch (err) {
+            
+        }
+    }
+}
+
+export { CreateMessageService }
