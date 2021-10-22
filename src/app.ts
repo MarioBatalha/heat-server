4dd9d53e9b5f9ed263b3cf68eb9c91d109b7ac5e
MarioBatalha <mariobatalha9@gmail.com>
2021-10-22 03:52:37 +0100

    add profileUser and getLast3Messages

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
 import "dotenv/config";
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 import { router } from "./routes";
 const app = express();
+app.use(cors());
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", socket => {
+  console.log(`Usuário conectado no socket ${socket.id}`)
+})
 
 app.use(express.json());
 app.use(router);
@@ -18,4 +33,5 @@ app.get("/signin/callback", (request, response) => {
 
   return response.json(code);
 });
-app.listen(4000, () => console.log("Server is running"));
+
+export { serverHttp, io }
